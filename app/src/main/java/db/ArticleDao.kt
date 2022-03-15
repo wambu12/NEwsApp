fbafbdc34d695e5046b740c7de7ded83a612a1d2
wambu12 <wambu86496@gmail.com>
2022-03-15 09:51:26 +0300

    Dao interface defined

--- app/src/main/java/db/ArticleDao.kt
+++ app/src/main/java/db/ArticleDao.kt
@@ -1,14 +1,24 @@
 package db
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.david.newsapp.ui.Article
 
 @Dao
 interface ArticleDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
 
+    @Query("SELECT * FROM articles")
 
+    fun getAllArticles(): LiveData<List<Article>>
 
-
-
-    
+    @Delete
+    suspend fun deleteArticle(article: Article)
 }
+
+
+
+
+
